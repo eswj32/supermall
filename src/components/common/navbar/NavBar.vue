5727e0440c3e8cda290c2a3bc0ccbe3740301d24
eswj32 <1848778679@qq.com>
2021-04-07 22:46:10 +0800

    完成1

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -16,14 +16,18 @@ export default {
 <style  scoped>
   .nav-bar {
     display: flex;
+    height: 44px;
     line-height: 44px;
+    text-align: center;
+    box-shadow: 0 1px 1px rgba(100, 100, 100, .1);
   }
-  .left .right {
+  .left, .right {
     width: 60px;
-    background-color: red;
+    /* background-color: red; */
   }
   .center {
     flex: 1;
-    background-color: blue;
+    /* background-color: blue; */
+    /* text-align: center; */
   }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="home">
-    <!-- <nav-bar></nav-bar> -->
+    <nav-bar class="home-nav"><div slot="center">首页</div></nav-bar>
   </div>
 </template>
 
 <script>
-// import NavBar from 'components/commom/navbar/NavBar'
-// import NavBar from 'components/commen/navbar/NavBar'
-
+import NavBar from 'components/common/navbar/NavBar'
 export default {
   name: "Home",
   components: {
-    // NavBar,
+    NavBar,
   },
 };
 </script>
 
 <style  scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
